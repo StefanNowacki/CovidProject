c603252d48e15f493bff30a40ad8093ce4273be9
StefanNowacki <79743793+StefanNowacki@users.noreply.github.com>
2022-03-02 15:06:42 +0100

    Update covidProject.sql

--- covidProject.sql
+++ covidProject.sql
@@ -1,94 +1,167 @@
-select location, data, total_cases, total_deaths, (total_deaths/ total_cases)*100 as DeathPercentage
-from coviddeaths
-where location like 'Poland'
-order by 1,2;
+SELECT
+    location,
+    data,
+    total_cases,
+    total_deaths,
+    ( total_deaths / total_cases ) * 100 AS deathpercentage
+FROM
+    coviddeaths
+WHERE
+    location LIKE 'Poland'
+ORDER BY
+    1,
+    2;
 
+--  total cases vs population
+--  percentage of population got covid
 
--- looking at total cases vs population
--- shows percentage of population got covid
-select location, data, total_cases,population , total_deaths, (total_cases/population)*100 as PopulationGotCovid
-from coviddeaths
-where location like 'Poland'
-order by 1,2;
-
+SELECT
+    location,
+    data,
+    total_cases,
+    population,
+    total_deaths,
+    ( total_cases / population ) * 100 AS populationgotcovid
+FROM
+    coviddeaths
+WHERE
+    location LIKE 'Poland'
+ORDER BY
+    1,
+    2;
 
 -- highest infection count by country
-select location, population, MAX(total_cases) as HighestInfectionCount, 
-MAX((total_cases/ population))* 100 as PercentPopulationInfected
-from coviddeaths
-group by location, population
-order by 4 desc;
 
+SELECT
+    location,
+    population,
+    MAX(total_cases) AS highestinfectioncount,
+    MAX((total_cases / population)) * 100 AS percentpopulationinfected
+FROM
+    coviddeaths
+GROUP BY
+    location,
+    population
+ORDER BY
+    4 DESC;
 
 -- highest deaths by country
-select location, max(total_deaths)as TotalDeathCount
-from coviddeaths
-where continent is not null and total_deaths is not null
-group by location
-order by TotalDeathCount desc;
 
+SELECT
+    location,
+    MAX(total_deaths) AS totaldeathcount
+FROM
+    coviddeaths
+WHERE
+    continent IS NOT NULL
+    AND total_deaths IS NOT NULL
+GROUP BY
+    location
+ORDER BY
+    totaldeathcount DESC;
 
 -- deaths by continent
-select continent, max(total_deaths)as TotalDeathCount
-from coviddeaths
-where continent is not null
-group by continent
-order by TotalDeathCount desc;
 
+SELECT
+    continent,
+    MAX(total_deaths) AS totaldeathcount
+FROM
+    coviddeaths
+WHERE
+    continent IS NOT NULL
+GROUP BY
+    continent
+ORDER BY
+    totaldeathcount DESC;
 
 -- global numbers
-select data, sum(new_cases) as total_cases, sum(new_deaths) as total_deaths,
-sum(new_deaths)/ sum(new_cases) * 100 as DeathPercentage
-from coviddeaths
+
+SELECT
+    data,
+    SUM(new_cases) AS total_cases,
+    SUM(new_deaths) AS total_deaths,
+    SUM(new_deaths) / SUM(new_cases) * 100 AS deathpercentage
+FROM
+    coviddeaths
 --where continent is not null
-group by data;
+GROUP BY
+    data;
 --order by 1,2;
 
 -- creating CTE for further calculation
-with PopulationVsVaccination (Continent, Location, Data, population, new_vaccinations, 
-SumOfVaccinationByDate)
-as
-(
--- total population vs vaccinations ( Rolling number of vacinattion by location and date)
-select dea.continent, dea.location, dea.data, dea.population, vac.new_vaccinations,
-sum(vac.new_vaccinations) over (partition by dea.location order by dea.location, dea.data)
-as SumOfVaccinationByDate
-from covidDeaths dea
-join covidvaccination vac
-    on dea.location = vac.location
-    and dea.data = vac.data
-)
 
+WITH populationvsvaccination (
+    continent,
+    location,
+    data,
+    population,
+    new_vaccinations,
+    sumofvaccinationbydate
+) AS (
+-- total population vs vaccinations ( Rolling number of vacinattion by location and date)
+    SELECT
+        dea.continent,
+        dea.location,
+        dea.data,
+        dea.population,
+        vac.new_vaccinations,
+        SUM(vac.new_vaccinations) OVER(
+            PARTITION BY dea.location
+            ORDER BY
+                dea.location, dea.data
+        ) AS sumofvaccinationbydate
+    FROM
+        coviddeaths        dea
+        JOIN covidvaccination   vac ON dea.location = vac.location
+                                     AND dea.data = vac.data
+)
 
 -- My CTE
 -- Rolling percentage of number of vaccinations by location and date
-select Continent, Location, Data, Population, New_vaccinations, SumOfVaccinationByDate,
-(SumOfVaccinationByDate/population)*100 as PercentageOfVaccinationByPopulation
-from PopulationVsVaccination;
+SELECT
+    continent,
+    location,
+    data,
+    population,
+    new_vaccinations,
+    sumofvaccinationbydate,
+    ( sumofvaccinationbydate / population ) * 100 AS percentageofvaccinationbypopulation
+FROM
+    populationvsvaccination;
 --where location like 'Albania';
 
-
 -- creating table 
-create table PercentageOfVaccinationByPopulation
-(
-Continent varchar(255),
-Location varchar(255),
-Data date,
-population number,
-new_vaccinations number,
-SumOfVaccinationByDate number
+
+CREATE TABLE percentageofvaccinationbypopulation (
+    continent                VARCHAR(255),
+    location                 VARCHAR(255),
+    data                     DATE,
+    population               NUMBER,
+    new_vaccinations         NUMBER,
+    sumofvaccinationbydate   NUMBER
 );
 
-
 -- insert output of calculaton to new table
-insert INTO PercentageOfVaccinationByPopulation
-select dea.continent, dea.location, dea.data, dea.population, vac.new_vaccinations,
-sum(vac.new_vaccinations) over (partition by dea.location order by dea.location, dea.data)
-as SumOfVaccinationByDate
-from covidDeaths dea
-join covidvaccination vac
-    on dea.location = vac.location
-    and dea.data = vac.data;
 
+INSERT INTO percentageofvaccinationbypopulation
+    SELECT
+        dea.continent,
+        dea.location,
+        dea.data,
+        dea.population,
+        vac.new_vaccinations,
+        SUM(vac.new_vaccinations) OVER(
+            PARTITION BY dea.location
+            ORDER BY
+                dea.location, dea.data
+        ) AS sumofvaccinationbydate
+    FROM
+        coviddeaths        dea
+        JOIN covidvaccination   vac ON dea.location = vac.location
+                                     AND dea.data = vac.data;
 
-select * from PercentageOfVaccinationByPopulation
+SELECT
+    *
+FROM
+    percentageofvaccinationbypopulation
+
